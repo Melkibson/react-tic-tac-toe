14b76f3130bd8fbc9ca9a3232ef479c53e8712ce
Melkibson <melkiyamna@protonmail.com>
2020-07-29 15:08:52 +0200

    remove index.css to use styled-components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,1 +1,28 @@
-// import styled from 'styled-components';
+import React from 'react';
+import Square from './Square';
+// import BoardRow from './BoardRow';
+import styled from 'styled-components';
+
+const BoardRow = styled.div`
+    display: grid;
+    grid-template-columns: 30% 30% 30%; 
+`
+
+const Board = (props) => {
+    let cases = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+      return (
+        <BoardRow>
+        {cases.map((value, index) => {
+            return (<Square
+            key={index} 
+            value={props.squares[value]}
+            onClick={()=> {props.onClick(value)}}
+          />)
+        })}
+          
+        </BoardRow>
+      );
+    }
+
+export default Board;
+  
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,14 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.button `
-    display: block;
+    background: #fff;
+    border: 1px solid #999;
+    float: left;
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 34px;
+    height: 34px;
+    margin-right: -1px;
+    margin-top: -1px;
+    padding: 0;
+    text-align: center;
+    width: 34px;
 `
 
 const Square = (props) => {
     return (
-      <Button className="square" onClick={props.onClick}>
-        {props.value}
-      </Button>
+        <Button onClick={props.onClick}>
+          {props.value}
+        </Button>
+      
     );
 }
 
